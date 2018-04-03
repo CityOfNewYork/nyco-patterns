1a0658d4a7e929641d027a5d4f7c5a5a6cf384a7
devowhippit <devonhirth@gmail.com>
2018-04-03 18:57:57 -0400

    Remove comments

--- .app/sass.js
+++ .app/sass.js
@@ -8,31 +8,6 @@ const Path = require('path');
 const Fs = require('fs');
 
 /**
- * Constants
- */
-
-// const bundle = modules[0]Path.join(__dirname, '../', modules[0].outDir);
-// const name = modules[0].config.outFile;
-
-/**
- * Functions
- */
-
-// function render(err, result) {
-//   if (err) {
-//     console.log(`${err.formatted}`);
-//   } else {
-//     Fs.writeFile(`${bundle}${name}`, result.css, (err) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(`SASS written to ${bundle}${name}`);
-//       }
-//     });
-//   }
-// }
-
-/**
  * Init
  */
 
