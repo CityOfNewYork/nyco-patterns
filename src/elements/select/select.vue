cb4decdd62c5deb48605563c4b518c5988aed898
KISS <me@luisam.com>
2019-03-11 09:48:13 -0400

    Cleaned up Select markup

--- src/elements/select/select.vue
+++ src/elements/select/select.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <select class="select" v-model="selected" @input="event => { $emit('input', event.target.value) }">
-      <option v-for="option in options" :key="option.value" :value="option.value">
+    <select class='select' v-model='selected' @input='event => { $emit("input", event.target.value) }'>
+      <option v-for='option in options' :key='option.value' :value='option.value'>
         {{ option.text }}
       </option>
     </select>
   </div>
 </template>
 
-<style>
-  /* Styles should be written in SASS */
-  /* Use BEMIT syntax for attribute. Try not to nest style declarations */
-  /* .o-chart {} Objects are prefixed with o- */
-  /* .o-chart__title {} A nested child of .o-chart is delimited by __ */
-  /* .o-chart--horizontal {} Different .o-chart types are delimited by -- */
-  /* if you need to nest a selector, it should be very very specific */
-</style>
+<style></style>
 
 <script>
   export default {
