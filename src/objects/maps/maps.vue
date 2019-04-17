44ea1b87378c2da4da609273e1177a4327226690
KISS <me@luisam.com>
2019-04-17 10:43:01 -0400

    [Feat/Maps] Implemented layer toggling for boroughs and neighborhoods

--- src/objects/maps/maps.vue
+++ src/objects/maps/maps.vue
@@ -2,6 +2,7 @@
   <div>
     <p>Hello, I'm a map!</p>
     <div id='nyco-map' style='height: 400px; width: 500px'></div>
+    <div id='nyco-map-menu' class='pt-2'></div>
   </div>
 </template>
 
@@ -13,6 +14,7 @@
     data() {
       return {
         map: null,
+        mapLayers: [],
       };
     },
     mounted() {
@@ -28,14 +30,25 @@
       'data.neighborhoods': function () {
         this.createNeighborhoodLayer(this.map, this.data.neighborhoods);
       },
+      'mapLayers': function () {
+        this.toggleLayers(this.map, this.mapLayers);
+      },
     },
     methods: {
+      trackMapLayers(layerRef) {
+        if (!this.mapLayers.includes(layerRef)) {
+          this.mapLayers.push(layerRef);
+        } else {
+          throw new Error("Map layer already exists");
+        }
+      },
       createBoroughLayer(map, layerData) {
         if (!map || !layerData) {
           throw Error(`Required ${map ? 'layerData' : 'map'} param is empty`);
         }
 
         const layerRef = 'boroughs';
+        this.trackMapLayers(layerRef);
 
         map.on('load', function(e) {
           map.addSource(`${layerRef}`, {
@@ -60,6 +73,7 @@
         }
 
         const layerRef = 'neighborhoods';
+        this.trackMapLayers(layerRef);
 
         map.on('load', function(e) {
           map.addSource(`${layerRef}`, {
@@ -77,6 +91,42 @@
             }
           });
         });
+      },
+      toggleLayers(map, layersToToggle) {
+        const toggleableLayerIds = layersToToggle;
+        const layers = document.getElementById('nyco-map-menu');
+
+        // clear all child nodes before adding new ones
+        while (layers.firstChild) {
+          layers.removeChild(layers.firstChild);
+        }
+
+        for (let i = 0; i < toggleableLayerIds.length; i++) {
+          const id = toggleableLayerIds[i];
+          const link = document.createElement('a');
+
+          link.href = '#';
+          link.classList.add('active', 'inline-block', 'pr-1');
+          link.textContent = id;
+
+          link.onclick = function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            const clickedLayer = this.textContent;
+            const visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+            if (visibility === 'visible') {
+              map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+              this.classList.remove('active');
+            } else {
+              this.classList.add('active');
+              map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+            }
+          };
+
+          layers.appendChild(link);
+        }
       },
       initializeMap() {
         mapboxgl.accessToken = this.data.api_key;
