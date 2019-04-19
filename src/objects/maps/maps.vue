884f01dd2ba63ca91a2c319dd160e949d29ebfac
KISS <me@luisam.com>
2019-04-19 12:21:58 -0400

    [Feat/Maps] Implemented basic on map click interaction for zipcodes

--- src/objects/maps/maps.vue
+++ src/objects/maps/maps.vue
@@ -3,6 +3,11 @@
     <p>Hello, I'm a map!</p>
     <div id='nyco-map' style='height: 400px; width: 500px'></div>
     <div id='nyco-map-menu' class='pt-2'></div>
+    <div id="nyco-map-details">
+      Information from selected shape:
+      <span v-if='selectedZipcode'>{{selectedZipcode}}</span>
+      <span v-else>none</span>
+    </div>
   </div>
 </template>
 
@@ -15,6 +20,7 @@
       return {
         map: null,
         mapLayers: [],
+        selectedZipcode: null,
       };
     },
     mounted() {
@@ -27,12 +33,12 @@
       'data.zipcodes': function () {
         this.createZipcodeLayer(this.map, this.data.zipcodes);
       },
-      'data.boroughs': function () {
-        this.createBoroughLayer(this.map, this.data.boroughs);
-      },
-      'data.neighborhoods': function () {
-        this.createNeighborhoodLayer(this.map, this.data.neighborhoods);
-      },
+      // 'data.boroughs': function () {
+      //   this.createBoroughLayer(this.map, this.data.boroughs);
+      // },
+      // 'data.neighborhoods': function () {
+      //   this.createNeighborhoodLayer(this.map, this.data.neighborhoods);
+      // },
       'mapLayers': function () {
         this.toggleLayers(this.map, this.mapLayers);
       },
@@ -64,10 +70,41 @@
             'type': 'fill',
             'source': `${layerRef}`,
             'paint': {
+              'fill-outline-color': '#484896',
               'fill-color': '#6c88c1',
               'fill-opacity': 0.6
             }
           });
+
+          map.addLayer({
+            'id': `${layerRef}-highlighted`,
+            'type': 'fill',
+            'source': `${layerRef}`,
+            'paint': {
+              'fill-outline-color': '#000',
+              'fill-color': '#6c88c1',
+              'fill-opacity': 0.6
+            },
+            'filter': ['in', 'GEOID10', '']
+          });
+        });
+
+        map.on('click', (e) => {
+          // set bbox as reactangle area around clicked point
+          let bbox = [[e.point.x, e.point.y], [e.point.x, e.point.y]];
+          let features = map.queryRenderedFeatures(bbox, { layers: [`${layerRef}`] });
+
+          // run through the selected features and set a filter
+          // to match features with unique GEOID10 codes to activate
+          // the `zipcodes-highlighted` layer.
+          const filter = features.reduce(function(memo, feature) {
+            memo.push(feature.properties.GEOID10);
+            return memo;
+          }, ['in', 'GEOID10']);
+
+          // filter format: ['in', 'GEOID10', 'zip']
+          this.selectedZipcode = filter[2];;
+          map.setFilter(`${layerRef}-highlighted`, filter);
         });
       },
       createBoroughLayer(map, layerData) {
