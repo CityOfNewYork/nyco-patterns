0b82e48e163b9e101ab8ab255457fd7f4bb6b865
KISS <me@luisam.com>
2019-04-26 12:52:08 -0400

    [Feat/Maps] Code cleanup, more decoupling, styling added

--- src/objects/maps/maps.vue
+++ src/objects/maps/maps.vue
@@ -1,30 +1,7 @@
 <template>
   <div>
-    <p>Hello, I'm a map!</p>
     <div id='nyco-map' style='height: 400px; width: 500px'></div>
     <div id='nyco-map-menu' class='pt-2'></div>
-    <div id="nyco-map-details">
-      <h3>Layer details</h3>
-      <p><strong>map layers:</strong> {{mapLayers}}</p>
-      <p><strong>active layer:</strong> {{activeLayer}}</p>
-      <div>
-        <strong>selected zipcode:</strong>
-        <span v-if='selectedZipcode'>{{selectedZipcode}}</span>
-        <span v-else>none</span>
-      </div>
-
-      <div>
-        <strong>selected neighborhood:</strong>
-        <span v-if='selectedNeighborhood'>{{selectedNeighborhood}}</span>
-        <span v-else>none</span>
-      </div>
-
-      <div>
-        <strong>selected borough:</strong>
-        <span v-if='selectedBorough'>{{selectedBorough}}</span>
-        <span v-else>none</span>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -41,11 +18,8 @@
     data() {
       return {
         map: null,
-        mapLoaded: false,
         mapLayers: [],
-        selectedZipcode: null,
-        selectedNeighborhood: null,
-        selectedBorough: null,
+        mapLoaded: false,
         activeLayer: undefined,
       };
     },
@@ -56,170 +30,35 @@
       //destroy map
     },
     watch: {
-      layers: function () {
+      'layers': function () {
         const layers = this.layers;
 
         for (let i = 0; i < layers.length; i++) {
           const layer = layers[i];
-          this.createLayer(layer);
+          this.initializeLayer(layer);
 
           if (layer.default)
             this.activeLayer = layer.name;
 
-          if (this.mapLoaded) {
-            this.addSource(layer.name, layer.data, ['in', layer.filterBy, ''], '#6c88c1');
-          }
+          if (this.mapLoaded)
+            this.addLayer(layer);
         }
       },
       'mapLayers': function () {
-        this.toggleLayers(this.map, this.mapLayers);
+        if (this.mapLayers.length > 1)
+          this.enableLayerToggling(this.mapLayers);
       },
       'mapLoaded': function () {
         if (this.mapLoaded) {
           const layers = this.layers;
 
           for (let i = 0; i < layers.length; i++) {
-            const layer = layers[i];
-
-            this.addSource(layer.name, layer.data, ['in', layer.filterBy, ''], '#6c88c1');
+            this.addLayer(layers[i]);
           }
         }
       }
     },
     methods: {
-      createLayer(layer) {
-        this.trackMapLayers(layer.name);
-        this.registerOnClick(layer.name, layer.filterBy);
-      },
-      trackMapLayers(layerRef) {
-        if (!this.mapLayers.includes(layerRef)) {
-          this.mapLayers.push(layerRef);
-        }
-      },
-      addSource(layerRef, layerData, filter, fill) {
-        // add source if it doesn't already exist and layerData isn't empty
-        if (this.map.getLayer(layerRef) === undefined && Object.entries(layerData).length !== 0) {
-          const visibility = layerRef === this.activeLayer ? 'visible' : 'none';
-
-          this.map.addSource(layerRef, {
-            'type': 'geojson',
-            'data': layerData
-          });
-
-          this.map.addLayer({
-            'id': layerRef,
-            'type': 'fill',
-            'source': layerRef,
-            'paint': {
-              'fill-outline-color': '#484896',
-              'fill-color': fill,
-              'fill-opacity': 0.6
-            },
-            'layout': {
-              'visibility': visibility
-            }
-          });
-
-          this.map.addLayer({
-            'id': `${layerRef}-highlighted`,
-            'type': 'fill',
-            'source': layerRef,
-            'paint': {
-              'fill-outline-color': '#484896',
-              'fill-color': fill,
-              'fill-opacity': 0.6
-            },
-            'filter': filter ? filter : [],
-            'layout': {
-              'visibility': visibility
-            }
-          });
-        }
-      },
-      registerOnClick(layerRef, property) {
-        this.map.on('click', layerRef, (e) => {
-          // set bbox as reactangle area around clicked point
-          let bbox = [[e.point.x, e.point.y], [e.point.x, e.point.y]];
-          let features = this.map.queryRenderedFeatures(bbox, { layers: [layerRef] });
-
-          const filter = features.reduce(function(memo, feature) {
-            memo.push(feature.properties[property]);
-            return memo;
-          }, ['in', `${property}`]);
-
-         // filter format: ['in', property, our_output]
-          if (layerRef === 'zipcodes')
-            this.selectedZipcode = filter[2];
-          else if (layerRef === 'neighborhoods')
-            this.selectedNeighborhood = filter[2];
-          else if (layerRef === 'boroughs')
-            this.selectedBorough = filter[2]
-
-          // apply filter to highlighted area
-          this.map.setFilter(`${layerRef}-highlighted`, filter);
-        });
-      },
-      toggleLayers(map, layersToToggle) {
-        const toggleableLayerIds = layersToToggle;
-        const layers = document.getElementById('nyco-map-menu');
-        const activeLayer = this.activeLayer;
-        const appRef = this;
-
-        // clear all child nodes before adding new ones
-        while (layers.firstChild) {
-          layers.removeChild(layers.firstChild);
-        }
-
-        for (let i = 0; i < toggleableLayerIds.length; i++) {
-          const id = toggleableLayerIds[i];
-          const link = document.createElement('a');
-
-          link.href = '#';
-          link.textContent = id;
-          link.classList.add('inline-block', 'pr-1');
-
-          if (id === activeLayer)
-            link.classList.add('active');
-
-          // update layer visibility on map and link active state on link click
-          link.onclick = function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-
-            const clickedLayer = this.textContent;
-            let allLinks = layers.getElementsByTagName('a');
-
-            for (let i = 0; i < allLinks.length; i++) {
-              const layer = allLinks[i].textContent;
-              const layerVisibility = map.getLayoutProperty(layer, 'visibility');
-
-              // if our current layer is also our clicked layer
-              if (clickedLayer === layer) {
-                // if the layer is already visible, leave it alone
-                if (layerVisibility === 'visible')
-                  return;
-
-                // if it's not visible, make it visible and set it as our active layer
-                this.classList.add('active');
-                // update active layer
-                appRef.activeLayer = layer;
-
-                map.setLayoutProperty(layer, 'visibility', 'visible');
-                map.setLayoutProperty(`${layer}-highlighted`, 'visibility', 'visible');
-              } else {
-                // if our current layer is not our clicked layer
-                // remove the active class and set visibility to none
-                this.classList.remove('active');
-                map.setLayoutProperty(layer, 'visibility', 'none');
-                map.setLayoutProperty(`${layer}-highlighted`, 'visibility', 'none');
-                map.setFilter(`${layer}-highlighted`, null);
-              }
-            }
-          };
-
-          layers.appendChild(link);
-        }
-      },
       initializeMap() {
         const mapConfig = this.config;
 
@@ -233,6 +72,148 @@
 
         this.map.addControl(new mapboxgl.NavigationControl());
         this.map.on('load', () => this.mapLoaded = true);
+      },
+      initializeLayer(layer) {
+        this.trackLayer(layer.name);
+        this.filterOnClick(layer);
+      },
+      trackLayer(reference) {
+        if (!this.mapLayers.includes(reference))
+          this.mapLayers.push(reference);
+      },
+      filterOnClick(layer) {
+        const map = this.map;
+
+        map.on('click', layer.name, (e) => {
+          // set bbox as reactangle area around clicked point
+          let bbox = [[e.point.x, e.point.y], [e.point.x, e.point.y]];
+          let features = map.queryRenderedFeatures(bbox, { layers: [layer.name] });
+
+          const filter = features.reduce(function(memo, feature) {
+            memo.push(feature.properties[layer.filterBy]);
+            return memo;
+          }, ['in', `${layer.filterBy}`]);
+
+          // apply filter on highlighted layer to draw focus on selection
+          map.setFilter(`${layer.name}-highlighted`, filter);
+        });
+      },
+      addLayer(layer) {
+        // add source if it doesn't already exist and layer data isn't empty
+        if (this.map.getLayer(layer.name) === undefined && Object.entries(layer.data).length !== 0) {
+          const visibility = layer.name === this.activeLayer ? 'visible' : 'none';
+          const filter = layer.filterBy ? ['in', layer.filterBy, ''] : [];
+          const fill = this.generateFillColor();
+
+          this.map.addSource(layer.name, {
+            'type': 'geojson',
+            'data': layer.data
+          });
+
+          this.map.addLayer({
+            'id': layer.name,
+            'type': 'fill',
+            'source': layer.name,
+            'paint': {
+              'fill-outline-color': fill[0],
+              'fill-color': fill[1],
+              'fill-opacity': 0.7
+            },
+            'layout': {
+              'visibility': visibility
+            }
+          });
+
+          this.map.addLayer({
+            'id': `${layer.name}-highlighted`,
+            'type': 'fill',
+            'source': layer.name,
+            'paint': {
+              'fill-outline-color': fill[0],
+              'fill-color': fill[1],
+              'fill-opacity': 0.7
+            },
+            'filter': filter,
+            'layout': {
+              'visibility': visibility
+            }
+          });
+        }
+      },
+      generateFillColor() {
+        // NYCO colors, ['fill-outline', 'fill']
+        const colors = [
+          // dark blue
+          ['#2F334F', '#6E7085'],
+          // blue
+          ['#3155A6', '#6C88C1'],
+          // red
+          ['#2F334F', '#F2695D'],
+          // gray
+          ['#2F334F', '#ACAEB9'],
+          // orange
+          ['#3155A6', '#F9A137']
+        ];
+
+        return colors[Math.floor(Math.random() * colors.length)];
+      },
+      enableLayerToggling(layersToToggle) {
+        const $this = this;
+        const map = $this.map;
+        const activeLayer = $this.activeLayer;
+        const linkContainer = document.getElementById('nyco-map-menu');
+
+        // clear all links before appending new ones to prevent duplicates
+        while (linkContainer.firstChild) {
+          linkContainer.removeChild(linkContainer.firstChild);
+        }
+
+        for (let i = 0; i < layersToToggle.length; i++) {
+          const layerRef = layersToToggle[i];
+          const link = document.createElement('a');
+
+          link.href = '#';
+          link.textContent = layerRef;
+
+          if (layerRef === activeLayer)
+            link.classList.add('active');
+
+          // on click update link state, active layer reference, and layer visibility
+          link.onclick = function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            const selectedLayer = this.textContent;
+            const links = linkContainer.getElementsByTagName('a');
+
+            for (let i = 0; i < links.length; i++) {
+              const currentLink = links[i];
+              const currentLayer = currentLink.textContent;
+              const layerVisibility = map.getLayoutProperty(currentLayer, 'visibility');
+
+              if (currentLayer === selectedLayer) {
+                // return if layer is already selected
+                if (layerVisibility === 'visible' && currentLink.classList.contains('active'))
+                  return;
+
+                // set our current layer as the global active layer,
+                // add active class to link, set layer as visible
+                $this.activeLayer = currentLayer;
+                currentLink.classList.add('active');
+                map.setLayoutProperty(currentLayer, 'visibility', 'visible');
+                map.setLayoutProperty(`${currentLayer}-highlighted`, 'visibility', 'visible');
+              } else {
+                // remove links active class, set layer visibility to none and remove filters
+                currentLink.classList.remove('active');
+                map.setLayoutProperty(currentLayer, 'visibility', 'none');
+                map.setLayoutProperty(`${currentLayer}-highlighted`, 'visibility', 'none');
+                map.setFilter(`${currentLayer}-highlighted`, null);
+              }
+            }
+          };
+
+          linkContainer.appendChild(link);
+        }
       },
     }
   };
